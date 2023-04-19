ed15ade39964418786008557d8a6fd284bcb41dc
Sajid Ahmed <85191237+sazahm@users.noreply.github.com>
2023-04-19 23:03:07 +0530

    deploy commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import Home from "./Home";
+import Singlemovie from "./Singlemovie";
+import Error from "./Error";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+const App = () => {
+  return <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="movie/:id" element={<Singlemovie />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+    </BrowserRouter>
+  </div>;
+};
 
 export default App;
